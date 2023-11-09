3b5b3c2830fec5dc4e3c29d3d462d0ebf627560e
Nkseervi <nkseervi@gmail.com>
2023-11-09 12:38:17 +0530

    Removed User from Db as we are using KithubUser

--- kithub.api/Data/KithubDbContext.cs
+++ kithub.api/Data/KithubDbContext.cs
@@ -116,7 +116,6 @@
         }
 
         public DbSet<Product> Products { get; set; }
-        public DbSet<User> Users { get;set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
